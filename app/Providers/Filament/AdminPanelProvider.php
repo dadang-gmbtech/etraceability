3d1d7a8dd17fa6d8e18d5b44d9dcabb26125370f
dadang-gmbtech <dadangiskandar@mail.ugm.ac.id>
2026-06-24 13:27:18 +0700

    feat: add show/hide password toggle on login page

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,8 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
-            ->passwordsRevealable()
+            ->login(\App\Filament\Pages\Auth\Login::class)
             ->colors([
                 'primary' => Color::Amber,
             ])
