f8b428bd9c67c06fc36598c9083f787e15b25507
dadang-gmbtech <dadangiskandar@mail.ugm.ac.id>
2026-06-24 13:31:17 +0700

    fix: use Filament 5 Schemas Component namespace in Login

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -2,9 +2,9 @@
 
 namespace App\Filament\Pages\Auth;
 
-use Filament\Forms\Components\Component;
-use Filament\Forms\Components\TextInput;
 use Filament\Auth\Pages\Login as BaseLogin;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Component;
 
 class Login extends BaseLogin
 {
